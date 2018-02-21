4217990c246d05a68a96a84c958784405076d942
Benoit Corne <benoit.corne@gmail.com>
2018-02-21 15:29:41 +0100

    hashing id using SHA-1 (#8)
    To avoid ending up with an id not fitting the elasticsearch id size
    limit (512 bytes). This commit is hashing the former id using the
    SHA-1 algorithm. In the meantime, the class name was added to the
    pattern to avoid collision with tests having the same name in
    different classes.

--- src/test/groovy/com/ullink/testtools/elastic/models/ResultObjectTest.groovy
+++ src/test/groovy/com/ullink/testtools/elastic/models/ResultObjectTest.groovy
@@ -1,5 +1,6 @@
 package com.ullink.testtools.elastic.models
 
+import com.ullink.testtools.elastic.TestExportTask
 import spock.lang.Specification
 
 class ResultObjectTest extends Specification {
@@ -7,6 +8,11 @@ class ResultObjectTest extends Specification {
     def 'should create Test Object'() {
         expect:
         new Result() instanceof Result
+    }
+
+    def 'sha1 hash'() {
+        expect:
+        TestExportTask.sha1Hashed('ABCDEF') == '970093678b182127f60bb51b8af2c94d539eca3a'
     }
 
 }
